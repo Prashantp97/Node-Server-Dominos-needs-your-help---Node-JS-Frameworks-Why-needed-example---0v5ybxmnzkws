668c5d88aac7202d8e5a0f7d41618d1be84be9f4
Prashant Patil <97236302+Prashantp97@users.noreply.github.com>
2022-09-18 21:58:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,31 @@ const httpServer = http.createServer(handleServer);
 
 function handleServer(req, res) {
   
+    const responseData= {
+        phone: '18602100000',
+        email: 'guestcaredominos@jublfood.com'
+    };
+
+    switch(req.url){
+        case '/welcome' :
+            res.writeHead(200, {'Content-Type':'text/plain'});
+            res.end('Welcome to Dominos!')
+            break;
+
+        case'/contact':
+            res.writeHead(200,{'Content-Type':'application/json'});
+            res.end(JSON.stringify(responseData));
+            break;
+
+        default:
+            res.writeHead(404,{'Content-Type':'text/plain'});
+            res.end('404 error');
+            break;
+    }
 }
+
+httpServer.listen(8081,()=>{
+    console.log('Listening to post 8081')
+});
 
 module.exports = httpServer;
